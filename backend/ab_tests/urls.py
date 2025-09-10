660363993746feeaba87b7245d480a651e1e7b3c
Segun228 <segunperkele@gmail.com>
2025-09-10 18:51:35 +0300

    main routes added

---
+++ backend/ab_tests/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("mde/<int:dataset_id>/", views.CountMDEView.as_view(), name="count_mde"),
+    path("sample-size/<int:dataset_id>/", views.SampleSizeView.as_view(), name="sample_size"),
+
+    # Тесты на сравнение двух групп
+    path("z-test/<int:dataset_id>/", views.Z_TestView.as_view(), name="z_test"),
+    path("t-test/<int:dataset_id>/", views.T_TestView.as_view(), name="t_test"),
+    path("welch-test/<int:dataset_id>/", views.Welch_test_View.as_view(), name="welch_test"),
+    path("u-test/<int:dataset_id>/", views.U_test_View.as_view(), name="u_test"),
+    path("chi-square/<int:dataset_id>/", views.Chi_TestView.as_view(), name="chi_square_test"),
+    path("chi-square-2sample/<int:dataset_id>/", views.Chi_2Sample_TestView.as_view(), name="chi_square_2sample_test"),
+
+    # Непараметрические тесты
+    path("ks-test/<int:dataset_id>/", views.KS_test_View.as_view(), name="ks_test"),
+    path("ks-test-2sample/<int:dataset_id>/", views.KS_2Sample_test_View.as_view(), name="ks_test_2sample"),
+
+    # Тесты на нормальность
+    path("lilliefors-test/<int:dataset_id>/", views.Lilleforce_test_View.as_view(), name="lilliefors_test"),
+    path("shapiro-wilk-test/<int:dataset_id>/", views.Shap_Wilke_test_View.as_view(), name="shapiro_wilk_test"),
+    path("anderson-darling-test/<int:dataset_id>/", views.Anderson_Darling_test_View.as_view(), name="anderson_darling_test"),
+
+    # Остальные
+    path("anova/<int:dataset_id>/", views.ANOVA_View.as_view(), name="anova"),
+    path("regression-test/<int:dataset_id>/", views.Regression_test_View.as_view(), name="regression_test"),
+    path("bootstrap/<int:dataset_id>/", views.Bootstrap_View.as_view(), name="bootstrap"),
+    path("cuped/<int:dataset_id>/", views.Cuped_View.as_view(), name="cuped"),
+]
--- backend/ab_tests/views.py
+++ backend/ab_tests/views.py
@@ -1,3 +1,122 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from api.models import Distribution, Dataset
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from .serializers import DistributionSerializer, DatasetSerializer
+
+from .permissions import IsAdminOrDebugOrReadOnly
+
+from backend.authentication import TelegramAuthentication
+
+
+from rest_framework.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
+import logging
+
+from kafka_broker.utils import build_log_message
+
+from rest_framework.response import Response
+from rest_framework import mixins, generics
+
+from django.core.cache import cache
+from dotenv import load_dotenv
+
+from django.http import HttpResponseBadRequest
+
+import os
+import requests
+import pandas as pd
+import uuid
+from io import BytesIO
+load_dotenv()
+from rest_framework.views import APIView
+
+
+class AuthenticatedAPIView:
+    authentication_classes = [TelegramAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class SampleSizeView(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class CountMDEView(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class Z_TestView(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class T_TestView(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class Chi_TestView(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class Chi_2Sample_TestView(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class KS_test_View(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class KS_2Sample_test_View(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class U_test_View(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class Lilleforce_test_View(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class Shap_Wilke_test_View(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class Welch_test_View(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class Anderson_Darling_test_View(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class Bootstrap_View(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class Cuped_View(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class ANOVA_View(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class Regression_test_View(AuthenticatedAPIView, APIView):
+    def post(self, request, *args, **kwargs):
+        pass
